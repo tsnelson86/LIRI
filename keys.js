e662b7e0d01fefd838171c299d37d0bbf5ad6963
Trevor Nelson <tsnelson86@gmail.com>
2017-10-01 23:25:11 -0600

    Homework for LIRI bot - need help with API integration and package.json

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'rfZyUNX7sCxcbEbiitbGO1sHQ',
+  consumer_secret: 'Dj7FJpIIbzUlkiJgGFTOPjEEEFs6rLDiOIftrMDUNEUdel90Zp',
+  access_token_key: '90512594-0NrOSNqW2h8hX4BY4eo0iVUHgt2KGfBHWGN5MPK6c',
+  access_token_secret: 'Ha4ygI1EZRavaJMrUI6jrIpWpiSufQQDH9Al8gygKQTxv'
+};
+
+var spotifyKeys = {
+  id: 'a29f61acb38844f5a8fe8ec3bde4c6f1',
+  secret: '1df63b847c0d405eb5856fd65a9f198d'
+};
+
+module.exports = twitterKeys;
+
+module.exports = spotifyKeys;
---
+++ liri.js
@@ -0,0 +1,123 @@
+var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require("request");
+var fs = require("fs");
+var omdb = require("omdb");
+
+var input = process.argv[2];
+
+var titleInput = "";
+
+for (var i = 3; i < process.argv.length; i++){
+  if (i > 3 && i < process.argv.length){
+    titleInput = titleInput + "+" + process.argv[i];
+  } else {
+    titleInput = titleInput + process.argv[i];
+  }
+}
+
+if (input == "my-tweets") {
+	showTweets();
+} else if (input == "spotify-this-song"){
+	if (titleInput != "") {
+		spotifySong(titleInput);
+	} else {
+		spotifySong("The+Sign");
+	}
+} else if (input == "movie-this"){
+	if (titleInput != "") {
+		findMovie(titleInput);
+	} else {
+		findMovie("Mr.+Nobody");
+	}
+} else if (input == "do-what-it-says") {
+	doIt();
+} else {
+	console.log("Not an accepted command");
+}
+
+function showTweets () {
+	//var client = new twitter(keys.twitterKeys);
+	var client = new Twitter({
+		consumer_key: 'rfZyUNX7sCxcbEbiitbGO1sHQ',
+		consumer_secret: 'Dj7FJpIIbzUlkiJgGFTOPjEEEFs6rLDiOIftrMDUNEUdel90Zp',
+		access_token_key: '90512594-0NrOSNqW2h8hX4BY4eo0iVUHgt2KGfBHWGN5MPK6c',
+		access_token_secret: 'Ha4ygI1EZRavaJMrUI6jrIpWpiSufQQDH9Al8gygKQTxv'
+	});
+
+  var params = {screen_name: "trevornelson86", count: 20};
+  client.get('statuses/user_timeline', params, function(err, tweets, response){
+    if (!err) {
+      for (var i = 0; i < tweets.length; i++) {
+        var date = tweets[i].created_at;
+        console.log("Most Recent Tweet (Tweet " + (i+1) + ") from @trevornelson86: " + "\n" + tweets[i].text + " - Tweeted At: " + date.substring(0, 19) + "\n");
+      }
+    } else {
+      console.log('ERROR');
+      console.log(err);
+    }
+  });
+}
+
+function spotifySong (song) {
+	//var spotify = new Spotify(keys.spotifyKeys);
+	var spotify = new Spotify ({
+  	id: 'a29f61acb38844f5a8fe8ec3bde4c6f1',
+  	secret: '1df63b847c0d405eb5856fd65a9f198d'
+ 	});
+	spotify.search({ type: "track", query: song}, function(err, data){
+    if (!err) {
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        var spotifyData = data.tracks.items[i];
+        console.log("Artist: " + spotifyData.artists[0].name);
+        console.log("Song: " + spotifyData.name);
+        console.log("Preview URL: " + spotifyData.preview_url);
+        console.log("Album: " + spotifyData.album.name);
+        console.log("\n");
+      }
+    } else {
+      console.log('ERROR');
+      console.log(err);
+    }
+  });
+}
+
+function findMovie (movieTitle) {
+	var omdbURL = ("http://www.omdbapi.com/?apikey=40e9cece&t=" + movieTitle);
+	request(omdbURL, function (err, response, body){
+		if (!err) {
+			var omdbResponse = JSON.parse(body);
+		  console.log("Title: " + omdbResponse.Title);
+		  console.log("Year: " + omdbResponse.Year);
+		  console.log("IBDM Rating: " + omdbResponse.imdbRating);
+		  console.log("Rotten Tomatoes Rating: " + omdbResponse.Ratings[2].Value);
+		  console.log("Country: " + omdbResponse.Country);
+		  console.log("Language: " + omdbResponse.Language);
+		  console.log("Plot: " + omdbResponse.Plot);
+		  console.log("Actors: " + omdbResponse.Actors);
+		} else {
+      console.log('ERROR');
+      console.log(err);
+		}
+	});
+}
+
+function doIt () {
+	fs.readFile("random.txt", "utf8", function(err, data){
+		var random = data.split(",");
+		switch (random[0]){
+			case "my-tweets":
+				showTweets();
+				break;
+			case "spotify-this-song":
+				spotifySong(random[1]);
+				break;
+			case "movie-this":
+				findMovie(random[1]);
+				break;
+			default: console.log("Not an accepted command");
+		}
+	});
+}
+
